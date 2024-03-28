bbab8aa0854b7bb36cd42e33f358782e3bc237ec
ArushRaghav <ArushRaghav622@gmail.com>
2024-03-28 15:17:34 +0530

    made some changes

--- README.md
+++
@@ -1,4 +0,0 @@
-# MakeASketch
-Just click in the box to start drawing.
-The project is in preliminary phase.
-You can click on the box to start drawing.
---
+++ draw.js
@@ -0,0 +1,57 @@
+let currInk = 'rgb(17,66,50)';
+let currBack = 'rgb(247,246,187)';
+let currSize = 30;
+
+// grid generating function
+function gen(size , colo ){
+    console.log(size);
+    let bo = document.body.getElementsByClassName('box')[0];
+    for(let i=0;i<size*size;i++){
+        let nd = document.createElement('div');
+        nd.style.height = `${600/size}px`;
+        nd.style.width = `${600/size}px`;
+        nd.style.backgroundColor = colo;
+        nd.classList.add('content');
+        bo.appendChild(nd);
+    }
+}
+
+//taking input
+function drawing(ink){
+    let bo = document.body.getElementsByClassName('box')[0];
+    bo.addEventListener('mousemove',(event)=>{
+        if(event.buttons == 1)
+        event.target.style.backgroundColor = ink;
+        event.preventDefault();
+    });
+}
+
+function changingColor(){
+    let col = document.body.getElementsByClassName('colo')[0];
+    col.addEventListener('change',(event)=>{
+        currInk = event.target.value;
+        drawing(currInk);
+    });
+}
+
+function deleteNode(){
+    let bo = document.getElementsByClassName('box')[0];
+    while(bo.firstChild != null){
+        bo.firstChild.remove();
+    }
+}
+
+function changeBackground(){
+    let col = document.body.getElementsByClassName('colo')[1];
+    col.addEventListener('change',(event)=>{
+        deleteNode();
+        currBack = event.target.value;
+        gen(currSize,currBack);
+    });
+}
+
+//generating
+gen(currSize,currBack);
+drawing(currInk);
+changingColor();
+changeBackground();
